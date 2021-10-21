8acad7de96550be9b7c6e3b6ce7744d25345ce3b
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-10-21 23:37:48 +0300

    add x0

--- 1/main.cpp
+++ 1/main.cpp
@@ -8,15 +8,15 @@ class DiffEquation {
   public:
   DiffEquation(function<float(float, float)> f, float x0, float y0, float step)
       : f(f), x_n(x0), y_n(y0), h(step){};
-  float next() {
+  pair<float, float> next() {  // y_(n+1)
     float k1 = f(x_n, y_n);
     float k2 = f(x_n + h / 2, y_n + h / 2 * k1);
     float k3 = f(x_n + h / 2, y_n + h / 2 * k2);
     float k4 = f(x_n + h, y_n + h * k3);
     y_n = y_n + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
     x_n += h;
-    return y_n;
-  }  // y_(n+1)
+    return {x_n, y_n};
+  }
 
   private:
   function<float(float, float)> f;  /// y' = f(x, y)
@@ -35,7 +35,10 @@ int main() {
   cout << "y' = f(x, y) = x * cos(y), x0 = " << x0 << ", y0 = " << y0
        << ", step = " << step << ", total steps = " << totalSteps << endl;
   DiffEquation de{f, x0, y0, step};
-  for (int i = 0; i < totalSteps; ++i)
-    cout << "y_" << i + 1 << " = " << de.next() << endl;
+  for (int i = 1; i <= totalSteps; ++i) {
+    auto point = de.next();
+    cout << "(x_" << i << ", y_" << i << ") = "
+         << "(" << point.first << ", " << point.second << ")" << endl;
+  }
   return 0;
 }
