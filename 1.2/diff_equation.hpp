6a4a73f584e21a3dabaf87b292a7fe4c3bbcf67e
Anton Kurbatov <a.kurbatov@protonmail.com>
2021-11-13 01:57:35 +0300

    add 1.2

---
+++ 1.2/diff_equation.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <functional>
+#include <vector>
+
+struct Point {
+  float x;
+  std::vector<float> y;
+
+  Point() = default;
+  Point(float x, std::vector<float> y) : x(x), y(y){};
+};
+
+class DiffEquation {
+  public:
+  DiffEquation(std::vector<std::function<float(float, float, float)>> system,
+               float x0,
+               std::vector<float> y0i,
+               float step);
+  Point next();
+
+  private:
+  const int systemSize = 2;
+  const std::vector<std::function<float(float, float, float)>> system;
+  float xn;
+  std::vector<float> yn;
+  float h;
+};
